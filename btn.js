8959d108d385366d04a63ff6d7ac0dd71a63a722
Fintech79 <abdelrahman.faisal@gmail.com>
2018-05-26 11:26:25 -0700

    css

--- btn.js
+++ btn.js
@@ -1,3 +1,7 @@
 // alert hello wolrd when h1 element is clicked
 var test = document.getElementById('test');
 test.addEventListener("click", function () {alert ("My Skills");} );
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
